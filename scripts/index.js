f0a548beee525097dfebab4ce57a09cc792db261
Vladislav Serdyukov <pandaisback62@mail.ru>
2023-02-16 23:40:53 +0300

    first rewiev

--- scripts/index.js
+++ scripts/index.js
@@ -1,25 +1,24 @@
-let gallery = document.querySelectorAll('section.gallery article.gallery__element button.gallery__like');   
 let editProfile = document.querySelector('.profile__edit');
 let popup = document.querySelector('.popup');
 let closeEditProfile = document.querySelector('.popup__close');
 let profileName = document.querySelector('.profile__name')
 let profileJob = document.querySelector('.profile__profession')
-let nameInput = document.querySelector('.popup__name');
-let jobInput = document.querySelector('.popup__job');
+let nameInput = document.querySelector('.popup__text_value_name');
+let jobInput = document.querySelector('.popup__text_value_profession');
 let formElemnt = document.querySelector('.popup__container')
 
-nameInput.value = profileName.textContent
-jobInput.value = profileJob.textContent
 
 
-editProfile.addEventListener('click', function() {
+
+function openPopup() {
     popup.classList.add('popup_opened');
-})
+    nameInput.value = profileName.textContent
+    jobInput.value = profileJob.textContent
+}
 
-
-closeEditProfile.addEventListener('click', function(){
+function closePopup() {
     popup.classList.remove('popup_opened')
-})
+}
 
 function handleForm(evt){
     evt.preventDefault(); 
@@ -27,14 +26,19 @@ function handleForm(evt){
     profileName.textContent = nameInput.value;
     profileJob.textContent = jobInput.value;
 
-    popup.classList.remove('popup_opened')
+    closePopup();
 }
 
-
+editProfile.addEventListener('click', openPopup)
+closeEditProfile.addEventListener('click', closePopup)
 formElemnt.addEventListener('submit', handleForm)
+/*
 
+--Активное состояние кнопки like--
+
+let gallery = document.querySelectorAll('section.gallery article.gallery__element button.gallery__like');   
 for (let i=0; i<gallery.length; i++) {
     gallery[i].addEventListener('click', function(){
-        gallery[i].classList.toggle('gallery__like-active')
+        gallery[i].classList.toggle('gallery__like_active')
     })
-}
+}*/
