cfdc4436fa4e3d5bc6eacd78fe0ded8037e4f142
Vladislav Serdyukov <pandaisback62@mail.ru>
2023-02-25 14:05:32 +0300

    after first review

--- scripts/index.js
+++ scripts/index.js
@@ -19,48 +19,24 @@ const popupImage = document.querySelector('.popup__image');
 const popupImageCaption = document.querySelector('.popup__image-caption');
 const closeButtonZoom = document.querySelector('section.popup_value_image button.popup__close');
 const articleTemplate = document.querySelector('#gallery__element').content;
-let arrGallery = [
-    {
-      name: 'Архыз',
-      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
-    },
-    {
-      name: 'Челябинская область',
-      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
-    },
-    {
-      name: 'Иваново',
-      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
-    },
-    {
-      name: 'Камчатка',
-      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
-    },
-    {
-      name: 'Холмогорский район',
-      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
-    },
-    {
-      name: 'Байкал',
-      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
-    }
-  ];
+
 
 function createGallery(items){
     items.forEach((item) => {
-        let card = createCard(item.link, item.name);
+        const card = createCard(item.link, item.name);
         gallery.append(card);
     });
 }
 
 function createCard (image,name){
   const articleElement = articleTemplate.querySelector('.gallery__element').cloneNode(true);
-  articleElement.querySelector('.gallery__image').src = image;
+  const articleImage = articleElement.querySelector('.gallery__image');
+  articleImage.src = image;
   articleElement.querySelector('.gallery__caption').textContent = name;
-  articleElement.querySelector('.gallery__image').alt = name;
+  articleImage.alt = name;
   articleElement.querySelector('.gallery__like').addEventListener('click', (evt) => evt.currentTarget.classList.toggle('gallery__like_active'));
-  articleElement.querySelector('.gallery__delete').addEventListener('click', (evt) => evt.target.closest('.gallery__element').remove());
-  articleElement.querySelector('.gallery__image').addEventListener('click', () => {
+  articleElement.querySelector('.gallery__delete').addEventListener('click', () => articleElement.remove());
+  articleImage.addEventListener('click', () => {
     popupImage.src = image;
     popupImageCaption.textContent = name;
     popupImage.alt = name;
@@ -87,7 +63,7 @@ function handleForm(evt){
     profileName.textContent = nameInput.value;
     profileJob.textContent = jobInput.value;
 
-    popupEdit.classList.remove('popup_opened');
+    closePopup(popupEdit);
 }
 
 function openPopup(popup){
